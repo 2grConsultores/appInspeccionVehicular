faa77b01c8c6b1d1765173969ea09158a0b2b49d
Cuauhtémoc Velasco Segura <31630155+cuauhtemocvs@users.noreply.github.com>
2023-11-13 15:58:26 -0600

    Add styles and routes, update component and template

--- validacion-cruzada-vin/src/app/explore-container/resultado/resultado.component.ts
+++ validacion-cruzada-vin/src/app/explore-container/resultado/resultado.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CasosService } from '../../services/casos.service';
+import { Caso } from 'src/app/interfaces/caso.interfaces';
 
 @Component({
   selector: 'app-resultado',
@@ -7,8 +10,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultadoComponent  implements OnInit {
 
-  constructor() { }
+  mostrarResutlado:boolean = true;
+  muestraVisibles:boolean = false;
+  muestraObd:boolean = false;
+  muestraEngomado:boolean = false;
+  caso:number = 0; 
+  casoJson:Caso = {
+    _id: 0,
+    visibles: {
+      listaLecturas: [{
+        lectura:'',
+        resultado:''
+      }],
+      vin: ''
+    },
+    obd: {
+      vin: ''
+    },
+    nfc: {
+      vin: ''
+    },
+    resultado: {
+      riesgo: '',
+      color: '',
+      descripcion: '',
+      recomendacion: []
+    }
+  };
 
-  ngOnInit() {}
+  constructor(
+    private router: Router,
+    private CasosService: CasosService,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe( ({id}) => { // aqui obtengo el id del registro
+      this.obtenercasos(id);
+    })
+  }
+
+  validarCaptura(){
+    setTimeout(() => {
+      // Después de 3 segundos, mostrar el resultado
+      console.log('Validando captura');
+      this.mostrarResutlado = true;
+  
+    }, 3500);
+
+
+  }
+
+  linkCapturaHome(){
+    this.router.navigate(['home']);
+  }
+
+  obtenercasos(numeroCaso: number){
+    console.log('numero de caso',numeroCaso);
+    this.CasosService.getJSON().subscribe( (casos: Caso[]) => {
+      console.log('casos',casos);
+      const foundCaso = casos.find( (caso: Caso) => caso._id == numeroCaso);
+      if (foundCaso !== undefined) {
+        this.casoJson = foundCaso; // if foundCaso is not undefined, assign it to this.casoJson
+        console.log('casoJson',this.casoJson);
+        if(foundCaso.visibles.vin == foundCaso.obd.vin && foundCaso.visibles.vin == foundCaso.nfc.vin){
+          this.muestraVisibles = true;
+          this.muestraObd = false; 
+          this.muestraEngomado = false;}
+        else if(foundCaso.visibles.vin == foundCaso.obd.vin && foundCaso.visibles.vin != foundCaso.nfc.vin){
+          this.muestraVisibles = true;
+          this.muestraObd = false; 
+          this.muestraEngomado = true;}
+        else if(foundCaso.visibles.vin != foundCaso.obd.vin && foundCaso.visibles.vin == foundCaso.nfc.vin){
+          this.muestraVisibles = true;
+          this.muestraObd = true; 
+          this.muestraEngomado = false;
+        }
+        else if(foundCaso.visibles.vin != foundCaso.obd.vin && foundCaso.visibles.vin != foundCaso.nfc.vin && foundCaso.obd.vin == foundCaso.nfc.vin){
+          this.muestraVisibles = true;
+          this.muestraObd = true; 
+          this.muestraEngomado = false;
+        }
+        else if(foundCaso.visibles.vin != foundCaso.obd.vin && foundCaso.visibles.vin != foundCaso.nfc.vin && foundCaso.obd.vin != foundCaso.nfc.vin){
+          this.muestraVisibles = true;
+          this.muestraObd = true; 
+          this.muestraEngomado = true;
+        }
+        
+      } else {
+        console.log('No se encontro el caso');
+      }
+    });
+  }
 
 }

4d6562369c967c1482e2cefcbe3efb678a83ea56
yuritepetl <yuritepetl@gmail.com>
2025-02-21 13:39:53 -0600

    Update vehicle photo capture logic in explore container

--- src/app/explore-container/cap-ocr/cap-ocr.component.ts
+++ src/app/explore-container/cap-ocr/cap-ocr.component.ts
@@ -14,99 +14,114 @@ import { LoadingController } from '@ionic/angular';
   templateUrl: './cap-ocr.component.html',
   styleUrls: ['./cap-ocr.component.scss'],
 })
-export class CapOcrComponent  implements OnInit {
-  fotoPuerta:boolean = false;
-  fotoParabrisas:boolean = false;
-  mostrarResutlado:boolean = false;
-  vinOCR:string = '';
-  vinImageBase64:string = '';
-  validacionId:string = ''; 
+export class CapOcrComponent implements OnInit {
+  fotoPuerta: boolean = false;
+  fotoParabrisas: boolean = false;
+  mostrarResutlado: boolean = false;
+  vinOCR: string = '';
+  vinImageBase64: string = '';
+  validacionId: string = '';
   isAlertOpen = false;
   isAlertOpenInputs = false;
-  posicion:string = '';
-  mensajeAlert:string = '';
+  posicion: string = '';
+  mensajeAlert: string = '';
   vinREGEX = /[a-hj-npr-zA-HJ-NPR-Z0-9]{17}/g;
-  mostrarTablaComparacion:boolean = false;
-  validacionData:validacionInt = {
+  mostrarTablaComparacion: boolean = false;
+  validacionData: validacionInt = {
     usuario: '',
     fechaInicio: new Date(),
     fechaFin: new Date(),
-    visibles:{
-      listaLecturas:[
+    visibles: {
+      listaLecturas: [
         {
-          posicion:'',
-          vinOCR:'',
-          vinEditado:'',
-          editado:false,
-          fecha:new Date(),
-          imagen:{
-            base64:'',
+          posicion: '',
+          vinOCR: '',
+          vinEditado: '',
+          editado: false,
+          fecha: new Date(),
+          imagen: {
+            base64: '',
           },
-        }
+        },
       ],
       vin: '',
     },
-    obd:{
-      vin: '',
-      fecha: new Date(),
-    },
-    nfc:{
-      vin: '',
-      fecha: new Date(),
-    },
-    resultado:{
-      riesgo: '',
-      color: '',
-      descripcion: '',
-      recomendacion: [],
-    }
-  };
-
-
-// resultados
-  mostrarVinPuerta:boolean = false;
-  resultadoVinPuerta:string = '';
-  mostrarVinParabrisas:boolean = false;
-  resultadoVinParabrisas:string = '';
-  mostrarVinFactura:boolean = false;
-  resultadoVinFactura:string = '';
-  mostrarVinTarjeta:boolean = false;
-  resultadoVinTarjeta:string = '';
-
-
-  //Captura de fotos
-capturaParabrisas:boolean = false;
-capturaPuerta:boolean = false;
-capturaFactura:boolean = false;
-capturaTarjetaCirculacion:boolean = false;
-
-// semaforos 
-mostrarIcono:boolean = false;
-colorResultado:string = '';
-iconoResultado:string = '';
-arregloResultados:any[] = [];
-
-  casoJson:Caso = {
-    _id: 0,
-    visibles: {
-      listaLecturas: [{
-        lectura:'',
-        resultado:''
-      }],
-      vin: ''
-    },
     obd: {
-      vin: ''
+      vin: '',
+      fecha: new Date(),
     },
     nfc: {
-      vin: ''
+      vin: '',
+      fecha: new Date(),
     },
     resultado: {
       riesgo: '',
       color: '',
       descripcion: '',
-      recomendacion: []
-    }
+      recomendacion: [],
+    },
+  };
+
+  // resultados
+  mostrarVinPuerta: boolean = false;
+  resultadoVinPuerta: string = '';
+  mostrarVinParabrisas: boolean = false;
+  resultadoVinParabrisas: string = '';
+  mostrarVinFactura: boolean = false;
+  resultadoVinFactura: string = '';
+  mostrarVinTarjeta: boolean = false;
+  resultadoVinTarjeta: string = '';
+
+  //Captura de fotos
+  capturaParabrisas: boolean = false;
+  capturaPuerta: boolean = false;
+  capturaFactura: boolean = false;
+  capturaTarjetaCirculacion: boolean = false;
+
+  capturaFrente: boolean = false;
+  capturaAtras: boolean = false;
+  capturaLadoIzquierdo: boolean = false;
+  capturaLadoDerecho: boolean = false;
+  capturaExterior1: boolean = false;
+  capturaExterior2: boolean = false;
+  capturaMotor: boolean = false;
+  capturaCajuela: boolean = false;
+  capturaTablero: boolean = false;
+  capturaInterior1: boolean = false;
+  capturaInterior2: boolean = false;
+  capturaInterior3: boolean = false;
+  capturaInterior4: boolean = false;
+  capturaInterior5: boolean = false;
+
+  // semaforos
+  mostrarIcono: boolean = false;
+  colorResultado: string = '';
+  iconoResultado: string = '';
+  arregloResultados: any[] = [];
+
+  casoJson: Caso = {
+    _id: 0,
+    visibles: {
+      listaLecturas: [
+        {
+          lectura: '',
+          resultado: '',
+        },
+      ],
+      vin: '',
+    },
+    obd: {
+      vin: '',
+    },
+    nfc: {
+      vin: '',
+    },
+    resultado: {
+      riesgo: '',
+      color: '',
+      descripcion: '',
+      recomendacion: [],
+    },
   };
 
   alertButtons = [
@@ -114,7 +129,7 @@ arregloResultados:any[] = [];
       text: 'Corregir',
       role: 'cancel',
       handler: () => {
-        console.log('vin ocr',this.vinOCR);
+        console.log('vin ocr', this.vinOCR);
         // this.setOpenInputs(true);
         this.presentAlertPrompt();
       },
@@ -124,8 +139,8 @@ arregloResultados:any[] = [];
       role: 'confirm',
       handler: () => {
         console.log('Alert confirmed');
-        console.log('vin ocr',this.vinOCR);
-        console.log('posicion',this.posicion);
+        console.log('vin ocr', this.vinOCR);
+        console.log('posicion', this.posicion);
         this.guardarValidoOCR(this.posicion, this.vinOCR);
         this.comparacionResultados();
       },
@@ -141,45 +156,46 @@ arregloResultados:any[] = [];
     private GoogleCloudVisionService: GoogleCloudVisionService,
     public alertController: AlertController,
     private loadingCtrl: LoadingController
-
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe( ({id}) => { // aqui obtengo el id del registro
+    this.activatedRoute.params.subscribe(({ id }) => {
+      // aqui obtengo el id del registro
       // this.obtenercasos(id);
       this.validacionId = id;
-      console.log('validacionId',this.validacionId);
+      console.log('validacionId', this.validacionId);
       this.obtenerDatosValidacion(this.validacionId);
-      });
-    
-      console.log('arreglo onInit',this.arregloResultados);
+    });
 
+    console.log('arreglo onInit', this.arregloResultados);
   }
 
-  comparacionResultados(){
-      // Limpiar el arreglo antes de agregar nuevos VIN
-      this.arregloResultados = [];
+  comparacionResultados() {
+    // Limpiar el arreglo antes de agregar nuevos VIN
+    this.arregloResultados = [];
     // Verificar si "listaLecturas" existe y tiene elementos
     if (this.validacionData.visibles.listaLecturas.length >= 2) {
       // Iterar sobre las lecturas
-      let i=0;
-      this.validacionData.visibles.listaLecturas.forEach(lectura => {
+      let i = 0;
+      this.validacionData.visibles.listaLecturas.forEach((lectura) => {
         // Verificar si "vinEditado" existe y tiene valor, en ese caso usarlo, de lo contrario usar "vinOCR"
         const vin = lectura.vinEditado ? lectura.vinEditado : lectura.vinOCR;
         // Agregar el VIN al arreglo
         this.arregloResultados.push(vin);
         i++;
-        console.log('vueltas:',i);
+        console.log('vueltas:', i);
       });
     }
-    console.log('arregloResultados',this.arregloResultados);
-    
-    if (this.validacionData.visibles.listaLecturas.length >= 1) { /// si el arreglo tiene 1 elemento muestra tabla
+    console.log('arregloResultados', this.arregloResultados);
+
+    if (this.validacionData.visibles.listaLecturas.length >= 1) {
+      /// si el arreglo tiene 1 elemento muestra tabla
       this.mostrarTablaComparacion = true;
     }
 
     const uniqueVins = new Set(this.arregloResultados);
-    if(this.arregloResultados.length >= 2){ // si el arreglo tiene 2 elementos muestra comparacion
+    if (this.arregloResultados.length >= 2) {
+      // si el arreglo tiene 2 elementos muestra comparacion
       if (uniqueVins.size === 1) {
         this.mostrarIcono = true;
         this.colorResultado = 'verde';
@@ -189,7 +205,7 @@ arregloResultados:any[] = [];
         this.colorResultado = 'rojo';
         this.iconoResultado = 'close-outline';
       }
-    } 
+    }
   }
 
   async presentAlertPrompt() {
@@ -200,7 +216,7 @@ arregloResultados:any[] = [];
           name: 'vin',
           type: 'text',
           value: this.vinOCR,
-        }
+        },
       ],
       buttons: [
         {
@@ -208,17 +224,17 @@ arregloResultados:any[] = [];
           role: 'cancel',
           handler: () => {
             console.log('Cancelado');
-          }
+          },
         },
         {
           text: 'Guardar',
           handler: (data) => {
             console.log('vin ingresado:', data.vin);
-            this.guardarEdicionVIN(this.posicion, data.vin );
+            this.guardarEdicionVIN(this.posicion, data.vin);
             this.comparacionResultados();
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
 
     await alert.present();
@@ -233,19 +249,18 @@ arregloResultados:any[] = [];
           role: 'cancel',
           handler: () => {
             console.log('Cancelado');
-          }
+          },
         },
-      ]
+      ],
     });
     await alert.present();
   }
 
-  validarCaptura(){
+  validarCaptura() {
     setTimeout(() => {
       // Después de 3 segundos, mostrar el resultado
       console.log('Validando captura');
       this.mostrarResutlado = true;
-  
     }, 3500);
   }
 
@@ -254,38 +269,41 @@ arregloResultados:any[] = [];
   }
 
   setOpenInputs(isOpen: boolean) {
-    console.log('setOpenInputs',isOpen);
+    console.log('setOpenInputs', isOpen);
     this.isAlertOpenInputs = isOpen;
   }
 
-  async capturaOCR(posicion: string){
-    await this.photoService.takePhoto().then( (base64Image: any) => {
-      console.log('base64Image',base64Image);
+  async capturaOCR(posicion: string) {
+    await this.photoService.takePhoto().then((base64Image: any) => {
+      console.log('base64Image', base64Image);
       this.vinImageBase64 = base64Image;
       const loading = this.showLoading();
-      this.GoogleCloudVisionService.getLabels(base64Image, 'TEXT_DETECTION').subscribe((result: any) => {
+      this.GoogleCloudVisionService.getLabels(
+        base64Image,
+        'TEXT_DETECTION'
+      ).subscribe((result: any) => {
         // console.log('result',result);
-        loading.then(loading => loading.dismiss());
+        loading.then((loading) => loading.dismiss());
         const vin = result.responses[0].textAnnotations[0].description;
         const matchesVinRegExp: any = vin.match(this.vinREGEX);
         // console.log('matchesVinRegExp',matchesVinRegExp); // matchesVinRegExp is an array
         // const esValido = this.vinREGEX.test(vin);
-        if (matchesVinRegExp !== null){
+        if (matchesVinRegExp !== null) {
           console.log('vin valido:', vin);
           this.vinOCR = matchesVinRegExp[0];
           this.arregloResultados.push(this.vinOCR);
           this.mensajeAlert = matchesVinRegExp[0];
           this.posicion = posicion;
-          if(posicion == 'puerta'){
+          if (posicion == 'puerta') {
             this.capturaPuerta = true;
-          } else if (posicion == 'parabrisas'){
+          } else if (posicion == 'parabrisas') {
             this.capturaParabrisas = true;
-          } else if (posicion == 'factura'){
+          } else if (posicion == 'factura') {
             this.capturaFactura = true;
-          } else if (posicion == 'tarjeta-circulacion'){
+          } else if (posicion == 'tarjeta-circulacion') {
             this.capturaTarjetaCirculacion = true;
           }
-          console.log('posicion:',this.posicion);
+          console.log('posicion:', this.posicion);
           this.setOpen(true);
         } else {
           console.log('vin invalido');
@@ -295,8 +313,58 @@ arregloResultados:any[] = [];
     });
   }
 
-  agregaFoto(posicion: string){
-    
+  async agregaFoto(posicion: string) {
+    await this.photoService.takePhoto().then((base64Image: any) => {
+      console.log('base64Image', base64Image);
+      this.vinImageBase64 = base64Image;
+      this.posicion = posicion;
+      switch (posicion) {
+        case 'frente':
+          this.capturaFrente = true;
+          break;
+        case 'atras':
+          this.capturaAtras = true;
+          break;
+        case 'lado-izquierdo':
+          this.capturaLadoIzquierdo = true;
+          break;
+        case 'lado-derecho':
+          this.capturaLadoDerecho = true;
+          break;
+        case 'exterior1':
+          this.capturaExterior1 = true;
+          break;
+        case 'exterior2':
+          this.capturaExterior2 = true;
+          break;
+        case 'motor':
+          this.capturaMotor = true;
+          break;
+        case 'cajuela':
+          this.capturaCajuela = true;
+          break;
+        case 'tablero':
+          this.capturaTablero = true;
+          break;
+        case 'interior1':
+          this.capturaInterior1 = true;
+          break;
+        case 'interior2':
+          this.capturaInterior2 = true;
+          break;
+        case 'interior3':
+          this.capturaInterior3 = true;
+          break;
+        case 'interior4':
+          this.capturaInterior4 = true;
+          break;
+        case 'interior5':
+          this.capturaInterior5 = true;
+          break;
+        default:
+          break;
+      }
+    });
   }
 
   async showLoading() {
@@ -307,31 +375,37 @@ arregloResultados:any[] = [];
     return loading;
   }
 
-  linkCapturaOBD(validacionId: string){
-    this.router.navigate(['obd/'+validacionId]);
+  linkCapturaOBD(validacionId: string) {
+    this.router.navigate(['obd/' + validacionId]);
   }
 
-  linkHome(validacionId: string){
+  linkHome(validacionId: string) {
     this.router.navigate(['home']);
   }
 
-  obtenerDatosValidacion(validacionId: string){
-    this.firestoreService.findOne('inspecciones', validacionId).subscribe( (validacion: validacionInt) => {
-      console.log('validacion Datos:',validacion);
-      this.validacionData = validacion;
-      } );
+  obtenerDatosValidacion(validacionId: string) {
+    this.firestoreService
+      .findOne('inspecciones', validacionId)
+      .subscribe((validacion: validacionInt) => {
+        console.log('validacion Datos:', validacion);
+        this.validacionData = validacion;
+      });
   }
 
-  guardarValidoOCR( posision: string, vinOCR: string){
-
-    const busquedaanteriores = this.validacionData.visibles.listaLecturas.filter( (lectura) => lectura.posicion == posision);
-    console.log('busquedaanteriores',busquedaanteriores);
-
-    if (busquedaanteriores.length >= 1) { // si ya existe una lectura en la posicion, la elimina
-      console.log('ya existe');
-      const indiceExistente = this.validacionData.visibles.listaLecturas.indexOf(
-        busquedaanteriores[0]
+  guardarValidoOCR(posision: string, vinOCR: string) {
+    const busquedaanteriores =
+      this.validacionData.visibles.listaLecturas.filter(
+        (lectura) => lectura.posicion == posision
       );
+    console.log('busquedaanteriores', busquedaanteriores);
+
+    if (busquedaanteriores.length >= 1) {
+      // si ya existe una lectura en la posicion, la elimina
+      console.log('ya existe');
+      const indiceExistente =
+        this.validacionData.visibles.listaLecturas.indexOf(
+          busquedaanteriores[0]
+        );
       if (indiceExistente !== -1) {
         this.validacionData.visibles.listaLecturas.splice(indiceExistente, 1);
       }
@@ -347,10 +421,14 @@ arregloResultados:any[] = [];
         base64: this.vinImageBase64,
       },
     };
-    
+
     this.validacionData.visibles.listaLecturas.push(nuevaLectura);
 
-    this.firestoreService.updateDoc(this.validacionData, 'inspecciones', this.validacionId);
+    this.firestoreService.updateDoc(
+      this.validacionData,
+      'inspecciones',
+      this.validacionId
+    );
 
     // Actualizar las variables de resultado y mostrar según la posición
     if (posision == 'puerta') {
@@ -369,35 +447,43 @@ arregloResultados:any[] = [];
     console.log('Guardado aprobo OCR');
   }
 
-  guardarEdicionVIN(posision: string, vinEditado: string ){
-  
-    const busquedaanteriores = this.validacionData.visibles.listaLecturas.filter( (lectura) => lectura.posicion == posision);
-    console.log('busquedaanteriores',busquedaanteriores);
-
-    if (busquedaanteriores.length >= 1) { // si ya existe una lectura en la posicion, la elimina
-      console.log('ya existe');
-      const indiceExistente = this.validacionData.visibles.listaLecturas.indexOf(
-        busquedaanteriores[0]
+  guardarEdicionVIN(posision: string, vinEditado: string) {
+    const busquedaanteriores =
+      this.validacionData.visibles.listaLecturas.filter(
+        (lectura) => lectura.posicion == posision
       );
+    console.log('busquedaanteriores', busquedaanteriores);
+
+    if (busquedaanteriores.length >= 1) {
+      // si ya existe una lectura en la posicion, la elimina
+      console.log('ya existe');
+      const indiceExistente =
+        this.validacionData.visibles.listaLecturas.indexOf(
+          busquedaanteriores[0]
+        );
       if (indiceExistente !== -1) {
         this.validacionData.visibles.listaLecturas.splice(indiceExistente, 1);
       }
     }
 
     const nuevaLectura = {
-          posicion: posision,
-          vinOCR: this.vinOCR,
-          vinEditado: vinEditado,
-          editado: true,
-          fecha: new Date(),
-          imagen: {
-            base64: this.vinImageBase64,
-          },
-        };
+      posicion: posision,
+      vinOCR: this.vinOCR,
+      vinEditado: vinEditado,
+      editado: true,
+      fecha: new Date(),
+      imagen: {
+        base64: this.vinImageBase64,
+      },
+    };
 
     this.validacionData.visibles.listaLecturas.push(nuevaLectura);
 
-    this.firestoreService.updateDoc(this.validacionData,'inspecciones',this.validacionId);
+    this.firestoreService.updateDoc(
+      this.validacionData,
+      'inspecciones',
+      this.validacionId
+    );
 
     if (posision == 'puerta') {
       this.mostrarVinPuerta = true;
